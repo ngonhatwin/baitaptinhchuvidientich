6b9ac97cf2db7989a490a288b35b7e9168c3b331
NgoNhatWin <117475856+ngonhatwin@users.noreply.github.com>
2022-11-21 09:33:01 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int a, b, chuvi, dientich;
+    printf ("\n hay nhap chieu dai hinh chu nhat= ");
+    scanf ("\n %d", &a);
+    printf ("\n hay nhap chieu rong hinh chu nhat = ");
+    scanf ("\n %d", &b);
+    
+    chuvi = (a+b)*2;
+    dientich = a*b;
+    
+    printf ("\n chuvi = %d", chuvi);
+    printf ("\n dientich = %d", dientich);
+}
